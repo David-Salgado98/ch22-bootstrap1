e21d4fbc74541d50a11d3ae8d0032cd89b63d462
davidsal <davidsalgad1998@gmail.com>
2023-01-10 11:30:49 -0600

    Agregamos hola y eventos

---
+++ eventos/hola.js
@@ -0,0 +1,8 @@
+function changeNombre(){
+    let nombre = prompt("Ingrese nombre");
+    //console.log(nombre);
+    //let etiqueta =
+     document.querySelector(".nombre").textContent =  nombre;
+    //console.log(etiqueta);
+    //etiqueta.textContent = nombre;
+}
